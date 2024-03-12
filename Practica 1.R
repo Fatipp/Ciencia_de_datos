068390f00e8a9de2c28f0ef8c0e2e87bebabb06a
Fatipp <162382578+Fatipp@users.noreply.github.com>
2024-03-12 17:41:47 -0600

    Practica 1
    Exploración de una matriz de datos (comandos básicos)

---
+++ Practica 1.R
@@ -0,0 +1,27 @@
+#___________ Práctica 1_______________
+# Exploración de datos
+
+
+# Se implementa la matriz iris que se encuentra cargada en R
+
+#--------------------------------------------------------
+#        Exploración de la Matriz  Iris
+#--------------------------------------------------------
+
+# 1.- Importación de la matriz
+data("CO2")
+data("iris")
+data(iris)
+
+# 2.- Dimensión de la matriz
+dim(iris)
+dim(CO2)
+# 3.- Nombre de las columnas
+colnames(iris)
+colnames(CO2)
+# 4.- Tipo de variables
+str(iris)
+str(CO2)
+# 5.- En busca de datos perdidos
+anyNA(iris)
+anyNA(CO2)
